ccf49be630c452fc9d1a4d76fdfce77f7d3d5ba6
Misty <gyc990326@gmail.com>
2023-01-23 00:53:47 +0800

    Support new version

--- patcher.py
+++ patcher.py
@@ -5,10 +5,23 @@ EXT_PATH = sys.argv[1]
 with open(EXT_PATH + '/background.js', 'r') as f:
     bgJSContent = f.read()
 
-bgJSContent_patched = bgJSContent.replace(
+bgJSContent_patched = bgJSContent
+
+
+# old
+bgJSContent_patched = bgJSContent_patched.replace(
     'function setChromePasswordSavingEnabled(e){',
     'function setChromePasswordSavingEnabled(e){ return;'
 )
+
+# new
+import re
+def replaceSetter1(text, propname):
+    return re.sub(r'this\.[^()]+?\(%s,!1\)' % (propname), r'null', text)
+
+bgJSContent_patched = replaceSetter1(bgJSContent_patched, 'chrome.privacy.services.passwordSavingEnabled')
+bgJSContent_patched = replaceSetter1(bgJSContent_patched, 'chrome.privacy.services.autofillCreditCardEnabled')
+bgJSContent_patched = replaceSetter1(bgJSContent_patched, 'chrome.privacy.services.autofillAddressEnabled')
 
 assert bgJSContent_patched != bgJSContent
 
